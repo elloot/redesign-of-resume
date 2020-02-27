7c8403d35b611bd9f80e5fcdf3d6bde5a144c531
elloot <elliot.duchek@gmail.com>
2020-02-27 15:21:34 +0100

    trying to animate the hamburger icon

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,12 @@
 (() => {
     let button = document.querySelector("#hamburger-toggle");
+    let hamburger = document.querySelectorAll("#hamburger");
     let menu = document.querySelector("#menu");
 
     button.addEventListener("click", e => {
         menu.classList.toggle("show-menu");
+        for (bar of hamburger) {
+            bar.classList.toggle("cross");
+        }
     });
 })();
