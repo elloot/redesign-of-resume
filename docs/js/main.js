f3db5e255a16430729970f4cedc0f3b3dc1d9c83
elloot <elliot.duchek@gmail.com>
2020-02-27 12:35:28 +0100

    change script to self invoking anonymous function expression

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,8 @@
-let button = document.querySelector("#hamburger-toggle");
-let menu = document.querySelector("#menu");
+(() => {
+    let button = document.querySelector("#hamburger-toggle");
+    let menu = document.querySelector("#menu");
 
-button.addEventListener("click", e => {
-    menu.classList.toggle("show-menu");
-});
+    button.addEventListener("click", e => {
+        menu.classList.toggle("show-menu");
+    });
+})();
