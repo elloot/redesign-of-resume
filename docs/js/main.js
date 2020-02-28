93f1d5b1870cc63a4cb98b6ddc730a48792c8e3e
elloot <elliot.duchek@gmail.com>
2020-02-28 08:21:09 +0100

    start fixing hamburger toggle animation

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,9 @@
 (() => {
     let button = document.querySelector("#hamburger-toggle");
-    let hamburger = document.querySelectorAll("#hamburger");
     let menu = document.querySelector("#menu");
 
     button.addEventListener("click", e => {
         menu.classList.toggle("show-menu");
-        for (bar of hamburger) {
-            bar.classList.toggle("cross");
-        }
+        button.classList.toggle("cross");
     });
 })();
