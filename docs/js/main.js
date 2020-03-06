602d46a2fb9b37ffa467eed92226755425a44ddb
elloot <elliot.duchek@gmail.com>
2020-03-06 15:24:54 +0100

    add JavaScript to restructure About me card on desktop to be able to mimic mockup in future

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,3 +7,26 @@
         button.classList.toggle("hamburger-cross");
     });
 })();
+
+(() => {
+    let title = document.querySelector("#aboutMeTitle"),
+        image = document.querySelector("#aboutMeImage"),
+        text = document.querySelector("#aboutMeText"),
+        container = document.querySelector("#aboutMeContainer");
+
+    function aboutMeRestructure(mediaQuery) {
+        if (mediaQuery.matches) {
+            //restructures the About me card for desktop
+            title.parentNode.removeChild(title);
+            container.insertBefore(title, text);
+        } else {
+            //restructures the About me card for smaller screens
+            title.parentNode.removeChild(title);
+            container.insertBefore(title, image);
+        }
+    }
+
+    let desktopQuery = window.matchMedia("(min-width: 992px)");
+    aboutMeRestructure(desktopQuery);
+    desktopQuery.addListener(aboutMeRestructure);
+})();
